1929de885914b19e880bd6197c8960a2d8f1e038
DrinoSan <s.becirbegovic@gmail.com>
2023-12-05 21:07:42 +0100

    Day04 part 2 - WOrk in progress

--- day04/main.cpp
+++ day04/main.cpp
@@ -1,10 +1,10 @@
 #include <algorithm>
 #include <fstream>
 #include <iostream>
-#include <ranges>
+#include <map>
+#include <sstream>
 #include <string>
 #include <vector>
-#include <sstream>
 
 #define INPUT "input.txt"
 #define TEST "test.txt"
@@ -24,6 +24,8 @@ int main() {
   }
 
   int32_t res;
+  std::map<int32_t, std::vector<int32_t>> commonElementsMap;
+  int32_t cardIndex = 1;
   for (auto &line : text) {
     auto pipe_position = std::find(line.begin(), line.end(), '|');
 
@@ -33,36 +35,54 @@ int main() {
     std::istringstream left_stream(left_side);
     std::istringstream right_stream(right_side);
 
-    std::vector<std::string> left_numbers{
-        std::istream_iterator<std::string>(left_stream),
-        std::istream_iterator<std::string>()};
+    std::vector<int32_t> left_numbers{
+        std::istream_iterator<int32_t>(left_stream),
+        std::istream_iterator<int32_t>()};
 
-    std::vector<std::string> right_numbers{
-        std::istream_iterator<std::string>(right_stream),
-        std::istream_iterator<std::string>()};
+    std::vector<int32_t> right_numbers{
+        std::istream_iterator<int32_t>(right_stream),
+        std::istream_iterator<int32_t>()};
+
+    std::sort(left_numbers.begin(), left_numbers.end());
+    std::sort(right_numbers.begin(), right_numbers.end());
+
+    std::vector<int32_t> commonElementsVec;
+
+    std::set_intersection(left_numbers.begin(), left_numbers.end(),
+                          right_numbers.begin(), right_numbers.end(),
+                          std::back_inserter(commonElementsVec));
+
+    commonElementsMap[cardIndex++] = commonElementsVec;
 
     size_t equalCount = std::count_if(
-        left_numbers.begin(), left_numbers.end(),
-        [&](const std::string& num) {
-            return std::find(right_numbers.begin(), right_numbers.end(), num) != right_numbers.end();
-        }
-    );
+        left_numbers.begin(), left_numbers.end(), [&](int32_t &num) {
+          return std::find(right_numbers.begin(), right_numbers.end(), num) !=
+                 right_numbers.end();
+        });
 
     int32_t tmp = 0;
-    for(int32_t i = 0; i < equalCount; ++i)
-    {
-        if( i > 0){
-            tmp *= 2; 
-        }
-        else {
-            tmp += 1;
-        }
+    for (int32_t i = 0; i < equalCount; ++i) {
+      if (i > 0) {
+        tmp *= 2;
+      } else {
+        tmp += 1;
+      }
     }
 
     res += tmp;
   }
 
-  std::cout << res << std::endl;
+  /* std::cout << res << std::endl; */
 
+  std::cout << "Common Eements for each card" << std::endl;
+  for (const auto [k, v] : commonElementsMap) {
+    std::cout << "Key: " << k << '\n';
+    std::cout << "\t\t";
+    for (const auto &e : v) {
+      std::cout << e << " ";
+    }
+    std::cout << std::endl;
+  }
+  std::cout << std::endl;
   return 0;
 }
